96702c473bbce163391a0993b0e4b19039349e1e
javaclass <javaclass@15000543cpu.fec.org>
2017-09-26 15:23:12 -0500

    My first calculator program

---
+++ src/basics/Calculator.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(calculateSum(45, 50));
+
+	}
+	
+	static int calculateSum(int number1, int number2){
+		return number1 + number2;
+	}
+
+}
