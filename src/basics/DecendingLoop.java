941837847fa1db704154c890b276dc83be76a32f
javaclass <javaclass@15000543cpu.fec.org>
2017-09-27 15:22:29 -0500

    Desending Int

---
+++ src/basics/DecendingLoop.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class DecendingLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int v = 10; 
+		  while(v >= 1) {
+		        System.out.println(v);  
+		        v = v-1;
+		  }
+		  
+		  
+	}
+
+}
