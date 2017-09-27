05caf2638d98679e2301168ee17abcf7dedab712
javaclass <javaclass@15000543cpu.fec.org>
2017-09-27 14:06:43 -0500

    grading program

---
+++ src/basics/CheckMarks.java
@@ -0,0 +1,33 @@
+package basics;
+
+public class CheckMarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gradeStudent(100);
+
+	}
+
+	public static void gradeStudent(int score) {
+		if (score >= 70 && score < 80) {
+			System.out.println("grade C");
+		}
+
+		else if (score <= 60 && score < 70) {
+			System.out.println("grade D");
+		}
+
+		else if (score < 60) {
+			System.out.println("grade F");
+		}
+
+		else if (score >= 80 && score < 90) {
+			System.out.println("grade B");
+		} else if (score >= 90) {
+			System.out.println("grade A");
+
+		}
+
+	}
+
+}
