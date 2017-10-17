6f9acd333ab3c55e509dd6df78eb0f62792a0e6a
javaclass <javaclass@15000543cpu.fec.org>
2017-10-17 15:33:00 -0500

    Homework2

--- src/basics/DivideTwoNumbers.java
+++ src/basics/DivideTwoNumbers.java
@@ -1,17 +1,17 @@
 package basics;
 import java.util.Scanner;
-public class Calculator {
+public class DivideTwoNumbers {
 
 	public static void main(String[] args) {
 	
 		int  a, b, c;
 	
-		System.out.println("Enter two numbers to calculate the difference of two numbers  ");
+		System.out.println("Enter two numbers to divide  ");
 		Scanner in = new Scanner(System.in);
 		a = in.nextInt();
 		b = in.nextInt();
-		c = a - b;
-		System.out.println("The difference between two numbers is " + c);
+		c = a / b;
+		System.out.println("The division of two numbers is " + c);
 	}
 
 }
