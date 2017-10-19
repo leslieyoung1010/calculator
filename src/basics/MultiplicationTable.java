947e9ffa84f67ac413a7df65727e176f810a53b3
javaclass <javaclass@15000543cpu.fec.org>
2017-10-19 09:24:59 -0500

    MultiplicationTable

---
+++ src/basics/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package basics;
+
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num =5;
+		 for(int i = 0; i < 10; i++)  {
+			 System.out.println(num + "X" +  i  + "=" + num*i );
+			}
+		
+		}
+
+	}
+
+
